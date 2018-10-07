a399f68940531685a4d2533ae7e15b9b5ea63921
Eric Togashi <eakt23@gmail.com>
2018-10-06 21:44:27 -0400

    Set up first box trigger to automatically open first door. (Door needs animation to open, sound still needed too)

---
+++ Subject 63/Assets/Scripts/LevelDynamics/BoxTriggerDoorOpen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTriggerDoorOpen : MonoBehaviour {
+
+    bool closed;
+    GameObject player;
+    Animator anim;
+    AudioSource source;
+    public AudioClip doorOpen;
+    public GameObject door;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        source = GetComponent<AudioSource>();
+        closed = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player && closed == true) {
+            anim.Play("Door Open");
+            source.PlayOneShot(doorOpen);
+        }
+    }
+}
