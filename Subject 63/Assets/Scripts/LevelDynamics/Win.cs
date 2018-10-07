f40c870b2b54bfb2f8b28bf37a4e9b0bd22a39bf
Eric Togashi <eakt23@gmail.com>
2018-10-06 22:07:38 -0400

    Box Collider for win condition is in place. It should work. Hasn't been tested yet, no player to test it

---
+++ Subject 63/Assets/Scripts/LevelDynamics/Win.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+
+    GameObject player;
+    SceneFaderInOut sceneFadeInOut;
+    float timer;
+    public float timeToEndLevel = 2f;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        sceneFadeInOut = GameObject.FindGameObjectWithTag(Tags.fader).GetComponent<SceneFaderInOut>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player) {
+            timer += Time.deltaTime;
+            if (timer >= timeToEndLevel)
+                sceneFadeInOut.EndScene();
+        }
+    }
+}
