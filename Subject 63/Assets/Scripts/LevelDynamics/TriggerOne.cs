a7d5c85156d9cf37d0dd88c1f9ed5b34e5eab6b8
Noah Kankanala <noahkankanala@Noahs-MacBook-Pro.local>
2018-10-08 14:14:07 -0400

    Added Box Trigger that triggers SFX

---
+++ Subject 63/Assets/Scripts/LevelDynamics/TriggerOne.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOne : MonoBehaviour {
+
+    GameObject player;
+    public AudioClip clip;
+    private AudioSource source;
+    void Awake()
+    {
+
+        source = GetComponent<AudioSource>();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            source.PlayOneShot(clip);
+        }
+    }
+}
+
