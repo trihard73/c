884172477d484502082e8885c22379a48818396b
DESKTOP-BD2SD2U\Owner <kkroush@iu.edu>
2018-10-09 11:49:56 -0400

    fixed player movement

--- Subject 63/Assets/PlayerControl.cs
+++ Subject 63/Assets/PlayerControl.cs
@@ -17,16 +17,43 @@ public class PlayerControl : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-
-
-    void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            rb.velocity = transform.forward * speed;
+        }
 
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+            rb.velocity = -transform.forward * speed;
+        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Rotate the sprite about the Y axis in the positive direction
+            rb.velocity = transform.right * speed;
+        }
 
-        rb.AddForce(movement * speed);
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Rotate the sprite about the Y axis in the negative direction
+            rb.velocity = -transform.right * speed;
+        }
     }
 }
+
+/*  void FixedUpdate()
+  {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+
+
+      Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+      rb.AddForce(movement * speed);
+  }
+}
+*/
